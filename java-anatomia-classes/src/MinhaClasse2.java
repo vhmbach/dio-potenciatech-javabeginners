7426af555b23abbfa0717f3ac04aa5c9746a2686
Vitor Bach <vitorhugo.bach@gmail.com>
2022-10-14 15:14:23 -0300

    anatomia de classe - 2

---
+++ java-anatomia-classes/src/MinhaClasse2.java
@@ -0,0 +1,8 @@
+public class MinhaClasse2 {
+    public static void main(String[] args) {
+        String meuNome = "Vitor";
+        int anoNascimento = 1998;
+        boolean verdadeiro = true;
+        boolean falso = false;
+    }
+}
